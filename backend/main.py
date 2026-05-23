a93fe9c15abf4d6bf64ed21b8f1c0232f66ca7b5
Henrique C. Rodrigues <henriquehoor18@hotmail.com>
2026-05-22 22:22:26 -0300

    feat: add função de baixa um backup do banco de dados

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,17 @@
+from backend.models import backup_db as backup
 from contextlib import asynccontextmanager
 from backend.models import database
 from backend.API import routes
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import asyncio
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     database.init_db()
+    task = asyncio.create_task(backup.agenda_backup())
     yield
+    task.cancel()
 
 app = FastAPI(title="API_Sistema_de_contas", lifespan=lifespan)
 
